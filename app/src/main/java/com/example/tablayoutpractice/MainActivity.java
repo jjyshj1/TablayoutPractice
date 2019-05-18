f0f044203ba00aa8a26108ffb1eef4aa0cffcd20
jjysh1 <jjyshj@kcpmed.com>
2019-05-18 11:02:04 +0900

    tablayout연습

--- app/src/main/java/com/example/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/example/tablayoutpractice/MainActivity.java
@@ -28,6 +28,17 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setValues() {
 
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("홈").setIcon(R.mipmap.ic_launcher));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("목록"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기1"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기2"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기3"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기4"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기5"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기6"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기7"));
+
     }
 
     @Override
