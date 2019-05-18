ec59c9ced199aa8d84e2b3511aebe99bae9fb308
jjysh1 <jjyshj@kcpmed.com>
2019-05-18 11:19:15 +0900

    custom tabview

--- app/src/main/java/com/example/tablayoutpractice/MainActivity.java
+++ app/src/main/java/com/example/tablayoutpractice/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.tablayoutpractice;
 import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.tablayoutpractice.databinding.ActivityMainBinding;
 
@@ -32,13 +35,21 @@ public class MainActivity extends BaseActivity {
         act.tabLayout.addTab(act.tabLayout.newTab().setText("목록"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("검색"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기1"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기2"));
-        act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기3"));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀1")));
+        act.tabLayout.addTab(act.tabLayout.newTab().setCustomView(createCustomTabView("커스텀2")));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기4"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기5"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기6"));
         act.tabLayout.addTab(act.tabLayout.newTab().setText("더보기7"));
 
+    }
+
+    View createCustomTabView(String tabName){
+        View tabView = LayoutInflater.from(mContext).inflate(R.layout.custom_tab,null);
+
+        TextView nameTxt = tabView.findViewById(R.id.nameTxt);
+        nameTxt.setText(tabName);
+        return tabView;
     }
 
     @Override
